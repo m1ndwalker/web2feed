55b50e7fe8188fa48e736ddc3353f63698f0afd2
Joel Alvim <mindwalkr@gmail.com>
2014-05-21 16:50:54 +0200

    Now supports crawling more than 1 crawler. The required crawlers must be passed in as a comma separated list of values in the --crawlers command line option. Each crawler is processed one after another. Since the sqlite database doesn't support concurrent writes this is meant to overcome issues when running more than 1 instance of web2feed in parallel. Now, only one instance should run but it goes through all the required crawlers.
    Use importlib.import_module instead of __import__ to instantiate the crawlers

--- web2feed.py
+++ web2feed.py
@@ -15,14 +15,15 @@ import urllib
 import random
 import logging
 import logging.config
+import importlib
 
 from bs4 import BeautifulSoup
 
 
 class Web2Feed:
 
-    def __init__(self, crawler, max_fetch_pages):
-        self._crawler = crawler
+    def __init__(self, crawlers, max_fetch_pages):
+        self._crawlers = crawlers
         self._max_fetch_pages = max_fetch_pages
 
 
@@ -46,173 +47,179 @@ class Web2Feed:
 
     def crawl(self):
 
-        conn, cur = self.prep_database()
+        for crawler in crawlers:
 
-        execution_time_str = (time.strftime("%Y%m%d-%H%M%S"))
+            self._logger.info("---------- Preparing to Crawl with %s" % crawler.plugin_name)
 
-        current_fetch_page = 1
-        last_id_found = False
-        last_id = None
+            conn, cur = self.prep_database()
 
-        cur.execute("select last_id from crawler_state where crawler=?", (self._crawler.plugin_name,))
-        last_id_row = cur.fetchone()
-        if last_id_row is None:
-            self._logger.info("Last Stored News ID for Crawler %s doesn't exist" % self._crawler.plugin_name)
-        else:
-            last_id = last_id_row[0]
-            self._logger.info("Last Stored News ID for Crawler %s is %s" % (self._crawler.plugin_name, last_id))
+            execution_time_str = (time.strftime("%Y%m%d-%H%M%S"))
 
+            current_fetch_page = 1
+            last_id_found = False
+            last_id = None
 
-        # Initialize the Random Seed, to be used throughout
-        random.random()
-
-        while last_id_found is not True and current_fetch_page <= self._max_fetch_pages:
-
-            req = urllib.request.Request(self._crawler.get_url_for_page(current_fetch_page),
-                                         headers={"User-Agent" : REQUEST_USER_AGENT_STRING})
-            f = urllib.request.urlopen(req)
-            #f = open("gitaarmarkt.htm", mode="r", encoding="utf-8")
-
-            soup = BeautifulSoup(f,"html5lib")
-
-            self._logger.info("Requesting crawler to process page %i" % current_fetch_page)
-
-            last_id_found = self._crawler.process_page(soup, last_id)
-
-            f.close()
-
-            # Retrieve the current records and save them to our database, then clean the array
-            # We want to be memory efficient
-
-            records = self._crawler.news_records
-
-            if len(records) == 0:
-                self._logger.info("No new search results found... ")
+            cur.execute("select last_id from crawler_state where crawler=?", (crawler.plugin_name,))
+            last_id_row = cur.fetchone()
+            if last_id_row is None:
+                self._logger.info("Last Stored News ID for Crawler %s doesn't exist" % crawler.plugin_name)
             else:
-                self._logger.info("Found %s new records" % len(records))
+                last_id = last_id_row[0]
+                self._logger.info("Last Stored News ID for Crawler %s is %s" % (crawler.plugin_name, last_id))
 
-                # Proceed to fetch the body of each record
-                for rec in records:
 
-                    # To avoid hitting the server always at the same interval, generate a random sleep time in secs
-                    sleep_time = random.randint(1,10)
-                    self._logger.info("In %i seconds, Fetching body for %s" % (sleep_time, rec.link))
+            # Initialize the Random Seed, to be used throughout
+            random.random()
 
+            while last_id_found is not True and current_fetch_page <= self._max_fetch_pages:
+
+                req = urllib.request.Request(crawler.get_url_for_page(current_fetch_page),
+                                             headers={"User-Agent" : REQUEST_USER_AGENT_STRING})
+                f = urllib.request.urlopen(req)
+                #f = open("gitaarmarkt.htm", mode="r", encoding="utf-8")
+
+                soup = BeautifulSoup(f,"html5lib")
+
+                self._logger.info("Requesting crawler to process page %i" % current_fetch_page)
+
+                last_id_found = crawler.process_page(soup, last_id)
+
+                f.close()
+
+                # Retrieve the current records and save them to our database, then clean the array
+                # We want to be memory efficient
+
+                records = crawler.news_records
+
+                if len(records) == 0:
+                    self._logger.info("No new search results found... ")
+                else:
+                    self._logger.info("Found %s new records" % len(records))
+
+                    # Proceed to fetch the body of each record
+                    for rec in records:
+
+                        # To avoid hitting the server always at the same interval, generate a random sleep time in secs
+                        sleep_time = random.randint(1,10)
+                        self._logger.info("In %i seconds, Fetching body for %s" % (sleep_time, rec.link))
+
+                        time.sleep(sleep_time)
+
+                        record_req = urllib.request.Request(rec.link,
+                                                            headers={"User-Agent" : REQUEST_USER_AGENT_STRING})
+                        record_f = urllib.request.urlopen(record_req)
+
+                        record_soup = BeautifulSoup(record_f,"html5lib")
+                        body_text = crawler.fetch_body_for(rec, record_soup)
+
+                        if not body_text is None and not body_text == "":
+                            rec.description = body_text
+
+
+                    for rec in records:
+                        self._logger.info("Inserting new record with ID: %s" % rec.id)
+                        cur.execute("INSERT INTO records VALUES (?,?,?,?,?,?,?)",
+                                    (rec.id,
+                                     rec.link,
+                                     rec.title,
+                                     rec.description,
+                                     time.mktime(rec.date.timetuple()),
+                                     time.time(),
+                                     crawler.plugin_name))
+
+                    # We iterate the pages from most recent to oldest, therefore our last found ID will be the
+                    # First item found, on the 1st page.
+                    if current_fetch_page == 1:
+                        self._logger.info("Saving Last Found ID: %s" % records[len(records) - 1].id)
+
+                        cur.execute("INSERT OR REPLACE into crawler_state(last_id, crawler) values (?,?)",
+                                    (records[len(records) - 1].id,crawler.plugin_name))
+
+                    # Clear the crawler list
+                    del crawler.news_records[:]
+
+                if last_id_found:
+                    self._logger.info("Last Stored News ID Found: " + last_id)
+                    break
+
+                self._logger.info("Processed page %s" % current_fetch_page)
+
+                sleep_time = random.randint(10,20)
+                # Wait 10-20 seconds before requesting the next page. We don't want to overload the server and get banned
+                # Don't always use the same period to appear more human
+                if current_fetch_page < self._max_fetch_pages:
+                    self._logger.info("Waiting %i seconds for next page..." % sleep_time)
                     time.sleep(sleep_time)
 
-                    record_req = urllib.request.Request(rec.link,
-                                                        headers={"User-Agent" : REQUEST_USER_AGENT_STRING})
-                    record_f = urllib.request.urlopen(record_req)
+                current_fetch_page += 1
 
-                    record_soup = BeautifulSoup(record_f,"html5lib")
-                    body_text = self._crawler.fetch_body_for(rec, record_soup)
+            # Commit results immediately after having fetched them all (there can be an error thrown
+            # in the output generation part and then nothing is saved to the database)
+            conn.commit()
 
-                    if not body_text is None and not body_text == "":
-                        rec.description = body_text
+            if writer == "htmlwriter":
+                results_file_name = os.path.normpath(out_path + "/" + crawler.plugin_name + "_results_" + execution_time_str +  ".htm")
 
+                if not last_id is None:
+                    cur.execute("select rowid from records where id=?", (last_id,))
 
-                for rec in records:
-                    self._logger.info("Inserting new record with ID: %s" % rec.id)
-                    cur.execute("INSERT INTO records VALUES (?,?,?,?,?,?,?)",
-                                (rec.id,
-                                 rec.link,
-                                 rec.title,
-                                 rec.description,
-                                 time.mktime(rec.date.timetuple()),
-                                 time.time(),
-                                 self._crawler.plugin_name))
+                    last_id_rowid = cur.fetchone()[0]
+                    cur.execute("SELECT id, link, title, description, datetime(date, 'unixepoch', 'localtime'), "
+                                "datetime(creation_date, 'unixepoch', 'localtime'), crawler "
+                                "from records where rowid > ?", (last_id_rowid,))
+                else:
+                    cur.execute("SELECT id, link, title, description, datetime(date, 'unixepoch', 'localtime'), "
+                                "datetime(creation_date, 'unixepoch', 'localtime'), crawler "
+                                "from records")
 
-                # We iterate the pages from most recent to oldest, therefore our last found ID will be the
-                # First item found, on the 1st page.
-                if current_fetch_page == 1:
-                    self._logger.info("Saving Last Found ID: %s" % records[len(records) - 1].id)
+                new_records = []
+                for row in cur:
 
-                    cur.execute("INSERT OR REPLACE into crawler_state(last_id, crawler) values (?,?)",
-                                (records[len(records) - 1].id,self._crawler.plugin_name))
+                    record = NewsRecord()
+                    record.id = row[0]
+                    record.link = row[1]
+                    record.title = row[2]
+                    record.description= row[3]
+                    record.date = datetime.datetime.strptime(row[4], "%Y-%m-%d %H:%M:%S")
+                    record.creation_date = datetime.datetime.strptime(row[5], "%Y-%m-%d %H:%M:%S")
+                    record.crawler_name = row[6]
 
-                # Clear the crawler list
-                del self._crawler.news_records[:]
+                    new_records.append(record)
 
-            if last_id_found:
-                self._logger.info("Last Stored News ID Found: " + last_id)
-                break
+                html_writer = HtmlWriter(new_records, results_file_name)
+                html_writer.write()
 
-            self._logger.info("Processed page %s" % current_fetch_page)
+            elif writer == "rsswriter":
+                results_file_name = os.path.normpath(out_path + "/" + crawler.plugin_name + ".xml")
 
-            sleep_time = random.randint(10,20)
-            # Wait 10-20 seconds before requesting the next page. We don't want to overload the server and get banned
-            # Don't always use the same period to appear more human
-            if current_fetch_page < self._max_fetch_pages:
-                self._logger.info("Waiting %i seconds for next page..." % sleep_time)
-                time.sleep(sleep_time)
+                # Get the last 200 records
+                rss_records = []
+                for row in cur.execute(
+                        "SELECT id, link, title, description, datetime(date, 'unixepoch', 'localtime'), "
+                        "datetime(creation_date, 'unixepoch', 'localtime'), crawler "
+                        "from records where crawler=? order by creation_date desc, id asc limit 200",
+                        (crawler.plugin_name,)):
 
-            current_fetch_page += 1
-
-        # Commit results immediately after having fetched them all (there can be an error thrown
-        # in the output generation part and then nothing is saved to the database)
-        conn.commit()
-
-        if writer == "htmlwriter":
-            results_file_name = os.path.normpath(out_path + "/" + self._crawler.plugin_name + "_results_" + execution_time_str +  ".htm")
-
-            if not last_id is None:
-                cur.execute("select rowid from records where id=?", (last_id,))
-
-                last_id_rowid = cur.fetchone()[0]
-                cur.execute("SELECT id, link, title, description, datetime(date, 'unixepoch', 'localtime'), "
-                            "datetime(creation_date, 'unixepoch', 'localtime'), crawler "
-                            "from records where rowid > ?", (last_id_rowid,))
-            else:
-                cur.execute("SELECT id, link, title, description, datetime(date, 'unixepoch', 'localtime'), "
-                            "datetime(creation_date, 'unixepoch', 'localtime'), crawler "
-                            "from records")
-
-            new_records = []
-            for row in cur:
-
-                record = NewsRecord()
-                record.id = row[0]
-                record.link = row[1]
-                record.title = row[2]
-                record.description= row[3]
-                record.date = datetime.datetime.strptime(row[4], "%Y-%m-%d %H:%M:%S")
-                record.creation_date = datetime.datetime.strptime(row[5], "%Y-%m-%d %H:%M:%S")
-                record.crawler_name = row[6]
-
-                new_records.append(record)
-
-            html_writer = HtmlWriter(new_records, results_file_name)
-            html_writer.write()
-
-        elif writer == "rsswriter":
-            results_file_name = os.path.normpath(out_path + "/" + self._crawler.plugin_name + ".xml")
-
-            # Get the last 200 records
-            rss_records = []
-            for row in cur.execute(
-                    "SELECT id, link, title, description, datetime(date, 'unixepoch', 'localtime'), "
-                    "datetime(creation_date, 'unixepoch', 'localtime'), crawler "
-                    "from records where crawler=? order by creation_date desc, id asc limit 200",
-                    (self._crawler.plugin_name,)):
-
-                #print(row)
+                    #print(row)
 
 
-                record = NewsRecord()
-                record.id = row[0]
-                record.link = row[1]
-                record.title = row[2]
-                record.description= row[3]
-                record.date = datetime.datetime.strptime(row[4], "%Y-%m-%d %H:%M:%S")
-                record.creation_date = datetime.datetime.strptime(row[5], "%Y-%m-%d %H:%M:%S")
-                record.crawler_name = row[6]
+                    record = NewsRecord()
+                    record.id = row[0]
+                    record.link = row[1]
+                    record.title = row[2]
+                    record.description= row[3]
+                    record.date = datetime.datetime.strptime(row[4], "%Y-%m-%d %H:%M:%S")
+                    record.creation_date = datetime.datetime.strptime(row[5], "%Y-%m-%d %H:%M:%S")
+                    record.crawler_name = row[6]
 
-                rss_records.append(record)
+                    rss_records.append(record)
 
-            rss_writer = RssWriter(rss_records, self._crawler.base_url, results_file_name)
-            rss_writer.write()
+                rss_writer = RssWriter(rss_records, crawler.base_url, results_file_name)
+                rss_writer.write()
 
-        conn.close()
+            conn.close()
+
+            self._logger.info("---------- Finished Crawling with %s" % crawler.plugin_name)
 
 #Max Fetch pages Default value is 5
 max_fetch_pages = 5
@@ -220,6 +227,9 @@ max_fetch_pages = 5
 writer = "htmlwriter"
 #Default path is the current directory
 out_path = os.path.normpath("./")
+
+# List containing the Instantiated Crawlers to crawl with
+crawlers = []
 
 REQUEST_USER_AGENT_STRING = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_6_8) AppleWebKit/537.13+ " \
                             "(KHTML, like Gecko) Version/5.1.7 Safari/534.57.2"
@@ -229,20 +239,29 @@ try:
     if len(sys.argv) <= 1:
         raise getopt.GetoptError("Not enough arguments")
 
-    opts, args = getopt.getopt(sys.argv[1:], "", ["crawler=", "max-fetch-p=", "writer=", "out-path="])
+    opts, args = getopt.getopt(sys.argv[1:], "", ["crawlers=", "max-fetch-p=", "writer=", "out-path="])
 except getopt.GetoptError:
     print("Usage: web2feed.py --crawler=crawler_to_use --max-fetch-p=max_number_pages_to_fetch "
           "--writer=writer_to_use --out-path=output_path_for_writer")
     sys.exit(2)
 for opt, arg in opts:
-    if opt == "--crawler":
-        try:
-            # The code below checks the package named after the crawler passed in as an argument
-            # and attempts to instantiate the expected class Crawler therein
+    if opt == "--crawlers":
 
-            crawler_plugin = __import__("crawlers." + arg, fromlist=["Crawler"])
-            crawler_construct = getattr(crawler_plugin, "Crawler")
-            crawler = crawler_construct()
+        crawlers_arg = arg.split(",")
+
+        try:
+
+            for crawler_arg in crawlers_arg:
+
+                # The code below checks the package named after the crawler is passed in as an argument
+                # and attempts to instantiate the expected class Crawler therein
+
+                crawler_plugin = importlib.import_module("crawlers." + crawler_arg)
+                crawler_construct = getattr(crawler_plugin, "Crawler")
+                crawler = crawler_construct()
+
+                crawlers.append(crawler)
+
         except ImportError as err:
             print("Error loading module: {0}".format(err))
             exit(1)
@@ -280,11 +299,11 @@ else:
 # Get the logger for this class
 logger = logging.getLogger("web2feed")
 
-logger.info("Configured crawler: %s" % crawler.plugin_name)
+logger.info("Configured crawlers: %s" % crawlers)
 logger.info("Max Pages to Fetch: %s" % max_fetch_pages)
 logger.info("Writer to use: %s" % writer )
 logger.info("Output Path: %s" % out_path )
 
-news_crawler = Web2Feed(crawler, int(max_fetch_pages))
+news_crawler = Web2Feed(crawlers, int(max_fetch_pages))
 news_crawler._logger =  logger
 news_crawler.crawl()
